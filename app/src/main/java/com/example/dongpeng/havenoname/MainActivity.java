39ea16983602470e85980938faacdd89eefbb23e
dongpeng <457055340@163.com>
2017-01-05 16:44:56 +0800

    Bmob后台服务

--- app/src/main/java/com/example/dongpeng/havenoname/MainActivity.java
+++ app/src/main/java/com/example/dongpeng/havenoname/MainActivity.java
@@ -3,25 +3,18 @@ package com.example.dongpeng.havenoname;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.widget.FrameLayout;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.example.dongpeng.havenoname.base.BaseActivity;
-import com.example.dongpeng.havenoname.entity.Person;
 import com.example.dongpeng.havenoname.my.MyFragment;
 
 public class MainActivity extends BaseActivity {
     private BottomNavigationBar bottom_navigation_bar;
-    private FrameLayout content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        content= (FrameLayout) findViewById(R.id.content);
-        Person p2 = new Person();
-        p2.setName("lucky");
-        p2.setPassword("北京海淀");
         bottom_navigation_bar= (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
         bottom_navigation_bar.setActiveColor(R.color.colorPrimary)
                 .setInActiveColor(R.color.colorGray);
--- app/src/main/java/com/example/dongpeng/havenoname/myapplication/MyApplication.java
+++ app/src/main/java/com/example/dongpeng/havenoname/myapplication/MyApplication.java
@@ -1,6 +1,9 @@
 package com.example.dongpeng.havenoname.myapplication;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobConfig;
@@ -9,21 +12,45 @@ import cn.bmob.v3.BmobConfig;
  * Created by dongpeng on 2017/1/4.
  */
 
-public class MyApplication extends Application{
+public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         //自v3.4.7版本开始,设置BmobConfig,允许设置请求超时时间、文件分片上传时每片的大小、文件的过期时间(单位为秒)，
-        BmobConfig config =new BmobConfig.Builder(this)
-        //设置appkey
-        .setApplicationId("fcba353a0de612e29df2064c6aff88e3")
-        //请求超时时间（单位为秒）：默认15s
-        .setConnectTimeout(15)
-        //文件分片上传时每片的大小（单位字节），默认512*1024
-        .setUploadBlockSize(1024*1024)
-        //文件的过期时间(单位为秒)：默认1800s
-        .setFileExpiration(2500)
-        .build();
+        BmobConfig config = new BmobConfig.Builder(this)
+                //设置appkey
+                .setApplicationId("fcba353a0de612e29df2064c6aff88e3")
+                //请求超时时间（单位为秒）：默认15s
+                .setConnectTimeout(15)
+                //文件分片上传时每片的大小（单位字节），默认512*1024
+                .setUploadBlockSize(1024 * 1024)
+                //文件的过期时间(单位为秒)：默认1800s
+                .setFileExpiration(2500)
+                .build();
         Bmob.initialize(config);
+    }
+
+    /**
+     * 获取版本号
+     *
+     * @return 当前应用的版本号
+     */
+    public static int getVersionCode(Context context) {
+        return getPackageInfo(context).versionCode;
+    }
+
+    private static PackageInfo getPackageInfo(Context context) {
+        PackageInfo pi = null;
+        try {
+            PackageManager pm = context.getPackageManager();
+            pi = pm.getPackageInfo(context.getPackageName(),
+                    PackageManager.GET_CONFIGURATIONS);
+
+            return pi;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return pi;
     }
 }
