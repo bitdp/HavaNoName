12725019c4ac5066f15a17f1c717e0b273b782f5
dongpeng <457055340@163.com>
2017-01-17 10:03:49 +0800

    viewFlipper

--- app/src/main/java/com/example/dongpeng/havenoname/home/HomeFragment.java
+++ app/src/main/java/com/example/dongpeng/havenoname/home/HomeFragment.java
@@ -8,10 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
-
 import com.example.dongpeng.havenoname.R;
 import com.example.dongpeng.havenoname.utils.ToastUtil;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,41 +18,50 @@ import java.util.List;
  */
 
 public class HomeFragment extends Fragment {
-    private List<String> data=new ArrayList<>();
+    private List<String> data = new ArrayList<>();
+    ViewFlipper vf;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.home_lay,container,false);
+        View view = inflater.inflate(R.layout.home_lay, container, false);
         initData();
-        ViewFlipper vf= (ViewFlipper) view.findViewById(R.id.vf);
-        for (int i = 0; i < data.size()/2; i++) {
-            View v=View.inflate(getActivity(),R.layout.flipper_item,null);
-            TextView tv1= (TextView) v.findViewById(R.id.tv1);
-            TextView tv2= (TextView) v.findViewById(R.id.tv2);
-            tv1.setText(data.get(i));
-            tv2.setText(data.get(i+1));
+        vf = (ViewFlipper) view.findViewById(R.id.vf);
+        if (data.size() % 2 == 1) {//数据源为偶数
+            data.add("");
+        }
+        addData();
+        vf.startFlipping();
+        return view;
+    }
+
+    private void addData() {
+        for (int i = 0; i < data.size() / 2; i++) {
+            View v = View.inflate(getActivity(), R.layout.flipper_item, null);
+            TextView tv1 = (TextView) v.findViewById(R.id.tv1);
+            TextView tv2 = (TextView) v.findViewById(R.id.tv2);
+            tv1.setText(data.get(2 * i));
+            tv2.setText(data.get(2 * i + 1));
             final int finalI = i;
             tv1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    ToastUtil.show(getActivity(),data.get(finalI));
+                    ToastUtil.show(getActivity(), data.get(2*finalI));
                 }
             });
             tv2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    ToastUtil.show(getActivity(),data.get(finalI+1));
+                    ToastUtil.show(getActivity(), data.get(2*finalI + 1));
                 }
             });
             vf.addView(v);
         }
-        vf.startFlipping();
-        return view;
     }
 
     private void initData() {
         for (int i = 0; i < 5; i++) {
-            data.add("这是第"+i+"条数据");
+            data.add("这是第" + i + "条数据");
         }
     }
 }
